5c06d19655f90264add6ffb962709b75986c0d7b
myeong <wjdaud5134@gmail.com>
2021-06-13 03:31:34 +0900

    Hello Github

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                Home
+            </div>
+        );
+    }
+}
+
+export default Home;
+
+// 참고: https://gist.github.com/velopert/21896de87566628b38b7ecb95973a7b3
